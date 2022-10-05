93a6772829341a12003b5c39575ad082c8651e7c
jakevc <jake.vancampen7@gmail.com>
2022-10-05 10:11:22 -1000

    publications

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,15 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Grayle',
+    description: `Grayle is a social platform that helps people find shoe communities and better fit`,
+    imgSrc: '/static/images/grayle.png',
+    href: 'https://grayle.co',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'PrimeLab',
+    description: `PrimeLab, the fastest blockchain ever`,
+    imgSrc: '/static/images/primelab.png',
+    href: 'https://primelab.io/',
   },
 ]
 
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -36,6 +36,58 @@ export default function Projects() {
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             Publications I have contributed to
           </p>
+          <p>
+            Escalona M, <b className="underline decoration-orange-500">VanCampen J</b>, Maurer NW,
+            Haukness M, Okhovat M, Harris RS, Watwood A, Hartley GA, O'Niel RJ, Medvedev P, Makova
+            KD, Vollmers C, Carbone L, Green RE. Whole-genome sequence and assembly of the Javan
+            gibbon (Hylobates moloch). <i>Journ. Heredit.</i>, esac043;{' '}
+            <a
+              href="https://doi.org/10.1093/jhered/esac043"
+              className="underline decoration-indigo-500"
+            >
+              doi: https://doi.org/10.1093/jhered/esac043.
+            </a>
+            .
+          </p>
+          <p>
+            Yashar WM, Kong G, <b className="underline decoration-orange-500">VanCampen J</b>, Smith
+            BM, Coleman DJ, Carbone L, Gürkan GY, Maxson JE, Braun TP. GoPeaks: Histone Modification
+            Peak Calling for CUT&TAG. bioRxiv 2022.01.475735;{' '}
+            <a
+              className="underline decoration-indigo-500"
+              href="https://doi.org/10.1101/2022.01.10.475735"
+            >
+              doi: https://doi.org/10.1101/2022.01.10.475735
+            </a>
+            .
+          </p>
+          <p>
+            Yashar WM, Smith BM, Coleman DJ,{' '}
+            <b className="underline decoration-orange-500">VanCampen J</b>, Kong G, Estabrook J,
+            Demir E, Long N, Bottomly D, McWeeney SK, Tyner JW, Durker BJ, Maxson JE, Braun TP. Dual
+            Targeting of FLT3 and LSD 1 Disrupts the MYC Super- Enhancer Complex in Acute Myeloid
+            Leukemia. BioRxiv 2022.01.17.476469;{' '}
+            <a
+              className="underline decoration-indigo-500"
+              href="https://doi.org/10.1101/2022.01.17.476469"
+            >
+              doi: https://doi.org/10.1101/2022.01.17.476469
+            </a>
+            .
+          </p>
+          <p>
+            Krishnan J, Seidel CW, Zhang N,{' '}
+            <b className="underline decoration-orange-500">VanCampen J</b>, Peuß R, Xiong S, Kenzir
+            A, Li H, Conway JW, Rohner N. Genome- wide analysis of cis-regulatory changes in the
+            metabolic adaptation of cavefish. BioRxiv 2020.08.27.270371;{' '}
+            <a
+              className="underline decoration-indigo-500"
+              href="https://doi.org/10.1101/2020.08.27.270371"
+            >
+              doi: https://doi.org/10.1101/2020.08.27.270371
+            </a>
+            .
+          </p>
         </div>
       </div>
     </>
